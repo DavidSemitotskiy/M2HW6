79286969603e34a7984d8aad4bf5140cec17fb18
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-02-16 19:27:40 +0200

    enums of cars were added

---
+++ M2HW6/BusinessCars.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2HW6
+{
+    public enum BusinessCars
+    {
+        FordMondeo,
+        KIAK5,
+        ToyotaCamry
+    }
+}
---
+++ M2HW6/CabrioletCars.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2HW6
+{
+    public enum CabrioletCars
+    {
+        LexusLC500,
+        MercedesBenzA238,
+        BMWM4
+    }
+}
