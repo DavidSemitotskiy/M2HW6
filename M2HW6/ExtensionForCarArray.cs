468d738146687e70f54ac34fdbe5ee6a8386d78d
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-02-16 19:22:52 +0200

    TaxiDepot and Extension for array and ITaxiDepot were added

---
+++ M2HW6/ExtensionForCarArray.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2HW6
+{
+    public static class ExtensionForCarArray
+    {
+        public static Car[] AddCar(this Car[] cars, Car car)
+        {
+            if (cars == null)
+            {
+                return null;
+            }
+
+            if (car == null)
+            {
+                return cars;
+            }
+
+            Car[] newCars = new Car[cars.Length + 1];
+            Array.Copy(cars, newCars, cars.Length);
+            newCars[cars.Length] = car;
+            return newCars;
+        }
+
+        public static Car[] DeleteCar(this Car[] cars, Car car)
+        {
+            if (cars == null)
+            {
+                return null;
+            }
+
+            if (car == null)
+            {
+                return cars;
+            }
+
+            Car[] newCars = new Car[cars.Length - 1];
+            int count = 0;
+            for (int i = 0, j = 0; j < cars.Length; j++)
+            {
+                if (cars[i].Name == car.Name)
+                {
+                    if (count == 0)
+                    {
+                        count++;
+                        continue;
+                    }
+                }
+
+                newCars[i] = cars[j];
+                i++;
+            }
+
+            return newCars;
+        }
+    }
+}
