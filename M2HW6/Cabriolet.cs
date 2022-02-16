6fe85c8708746bc72dd6bcb7c44a4bfe3ab20d1e
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-02-16 19:16:15 +0200

    add cars hierarchy and some enums

---
+++ M2HW6/Cabriolet.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2HW6
+{
+    public class Cabriolet : BusinessCar
+    {
+        public Cabriolet(string name, decimal price, decimal fuelConsumption, int countPassengerPlace, Rating rating)
+            : base(name, price, fuelConsumption, countPassengerPlace, rating)
+        {
+        }
+
+        public bool IsOpenRoof { get; private set; }
+
+        public override void Move()
+        {
+            Random random = new Random();
+            IsOpenRoof = random.Next(2) == 0 ? true : false;
+            if (IsOpenRoof)
+            {
+                Console.WriteLine("По велению пассажира крыша автомобиля была открыта!");
+            }
+            else
+            {
+                Console.WriteLine("По велению пассажира крыша автомобиля была закрыта!");
+            }
+
+            base.Move();
+        }
+
+        public override string GetDescription()
+        {
+            return $"{base.GetDescription()} : Открыта ли крыша - {IsOpenRoof}";
+        }
+    }
+}
---
+++ M2HW6/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2HW6
+{
+    public abstract class Car : IComparable<Car>
+    {
+        public Car(string name, decimal price, decimal fuelConsumption)
+        {
+            Name = name;
+            Price = price;
+            FuelConsumption = fuelConsumption;
+        }
+
+        public string Name { get; }
+        public decimal Price { get; }
+        public decimal FuelConsumption { get; }
+        public Gear Gear { get; private set; } = Gear.N;
+        public abstract void ChangeGear();
+
+        public virtual void Move()
+        {
+            Console.WriteLine("Я еду!");
+        }
+
+        public int CompareTo(Car? other)
+        {
+            return FuelConsumption.CompareTo(other.FuelConsumption);
+        }
+
+        public virtual string GetDescription()
+        {
+            return $"Марка - {Name} : Цена - {Price}$ : Расход топлива на 100 км - {FuelConsumption} л";
+        }
+    }
+}
